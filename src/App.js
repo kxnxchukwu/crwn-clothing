06c3d8bda028e7a788d7a08dcba2ebc7161af5e2
Kenechukwu <kenechukwu.ajufo@gmail.com>
2022-01-17 14:30:20 +0000

    Add Header Component with Links to Contact and Shop Pages, SVG Logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
     <BrowserRouter>
+    <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
