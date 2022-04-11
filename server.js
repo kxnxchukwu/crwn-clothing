e338490539e8010dba8e93dcd8d8ebd965118aa8
Kenechukwu <kenechukwu.ajufo@gmail.com>
2022-04-11 19:47:51 +0100

    Modified Express to use Express SSlify only in Production

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({trustProtoHeader: true}));
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({trustProtoHeader: true}));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
