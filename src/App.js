911c8d867d67aa227cf4c769a1936a43ff9396de
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-12 19:11:04 +0100

    Modified App Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Suspense, useEffect, lazy } from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import React, { Suspense, useEffect, lazy, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import Header from "./components/header/header.component";
 import { GlobalStyle } from "./global.styles";
 import { useSelector, useDispatch } from "react-redux";
@@ -7,45 +8,64 @@ import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
 import Spinner from "./components/spinner/spinner.component";
 import ErrorBoundary from "./components/error-boundary/error-boundary.component";
+import { lightTheme, darkTheme } from "./themes";
+import { fetchCollectionsStart } from "./redux/shop/shop.actions";
+import NotFound from "./components/notfound/notfound.component";
 
 const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
-const ShopPage = lazy(() => import("./pages/shop/shop.component"));
 const SignInAndSignUpPage = lazy(() =>
   import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
 );
 const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"));
+const ContactPage = lazy(() => import("./pages/contact/contactpage.component"));
+const CollectionsOverviewContainer = lazy(() =>
+  import("./components/collections-overview/collections-overview-container")
+);
+const CollectionPageContainer = lazy(() =>
+  import("./components/collection/collection.container")
+);
 
 const App = () => {
   const currentUser = useSelector(selectCurrentUser);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(checkUserSession());
+    dispatch(fetchCollectionsStart());
   }, [dispatch]);
-
+  const [theme, setTheme] = useState("light");
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
   return (
-    <div>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Header />
-        <Suspense fallback={<Spinner />}>
-          <Switch>
-            <ErrorBoundary>
-            
-              <Route exact path="/" component={HomePage} />
-              <Route path="/shop" component={ShopPage} />
-              <Route exact path="/checkout" component={CheckoutPage} />
-              <Route
-                exact
-                path="/signin"
-                render={() =>
-                  currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-                }
-              />
-            </ErrorBoundary>
-          </Switch>
-        </Suspense>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <div>
+        <BrowserRouter>
+          <ErrorBoundary>
+            <GlobalStyle />
+            <Header themeToggler={themeToggler} />
+            <Suspense fallback={<Spinner />}>
+              <Routes>
+                <Route exact path="/" element={<HomePage />} />
+                <Route path="/shop">
+                  <Route index element={<CollectionsOverviewContainer />} />
+                  <Route path=":id" element={<CollectionPageContainer />} />
+                </Route>
+                <Route exact path="/checkout" element={<CheckoutPage />} />
+                <Route exact path="/contact" element={<ContactPage />} />
+                <Route
+                  exact
+                  path="/signin"
+                  element={
+                    currentUser ? <Navigate to="/" /> : <SignInAndSignUpPage />
+                  }
+                />
+                <Route path="*" exact={true} element={<NotFound />} />
+              </Routes>
+            </Suspense>
+          </ErrorBoundary>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 };
 
