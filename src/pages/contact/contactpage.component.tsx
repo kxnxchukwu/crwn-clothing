8201351afa6e2ab0b09b25692e3d9a7431187bc5
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-12 18:55:27 +0100

    Add Contact Page Component

---
+++ src/pages/contact/contactpage.component.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import NotFound from "../../components/notfound/notfound.component";
+
+export default function ContactPage(): ReactElement {
+  return <NotFound message="This Page Is Under Construction" />;
+}
