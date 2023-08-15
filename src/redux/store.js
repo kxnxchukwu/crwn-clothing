3f9d27330738efd0a0170ccc5f4b9a730b53a474
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-15 10:31:57 +0100

    Ran Prettier to format file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from './root-saga';
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./root-saga";
 
-import rootReducer from './root-reducer';
+import rootReducer from "./root-reducer";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [sagaMiddleware];
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
