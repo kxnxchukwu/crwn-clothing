1786c3b4e78e206ab7eb69bb946ea7970c08943f
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-15 10:11:14 +0100

    Additional TS Conversion

---
+++ src/components/collection/collection.container.tsx
@@ -0,0 +1,35 @@
+import { ReactElement } from "react";
+import "./collection.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
+import { useSelector } from "react-redux";
+import {
+  selectCollection,
+  selectIsCollectionFetching,
+} from "../../redux/shop/shop.selectors";
+import { useParams } from "react-router-dom";
+import { Item } from "../../redux/cart/cart.actions";
+import Spinner from "../spinner/spinner.component";
+
+export default function CollectionPageContainer(): ReactElement {
+  const { id } = useParams();
+
+  const isFetching = useSelector(selectIsCollectionFetching);
+
+  const collection = useSelector(selectCollection(id as string));
+  if (isFetching) {
+    return <Spinner />;
+  }
+
+  console.log(collection);
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title.toUpperCase()}</h2>
+      <div className="items">
+        {items.map((item: Item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
