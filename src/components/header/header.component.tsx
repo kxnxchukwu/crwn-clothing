aaf27b91ef97c30c1dbec4cd0f5302c8383cb33b
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-15 10:28:49 +0100

    Converted Header Component to TypeScript

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,65 @@
+import { ReactElement } from "react";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import ThemeToggler from "../theme-toggler/ThemeToggler.component";
+import { useDispatch, useSelector } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCardHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+} from "./header.styles";
+import { signOutStart } from "../../redux/user/user.actions";
+import { useLocation } from "react-router-dom";
+
+export interface HeaderProps {
+  themeToggler: () => void;
+}
+
+export default function Header({ themeToggler }: HeaderProps): ReactElement {
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
+  const hidden = useSelector(selectCardHidden);
+  const currentUser = useSelector(selectCurrentUser);
+  return (
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo className="logo" />
+      </LogoContainer>
+      <OptionsContainer className="options">
+        <>
+          <ThemeToggler themeToggler={themeToggler} />
+        </>
+        <OptionLink
+          active={pathname === "/shop" ? "active" : undefined}
+          to="/shop"
+        >
+          SHOP
+        </OptionLink>
+        <OptionLink
+          active={pathname === "/contact" ? "active" : undefined}
+          to="/contact"
+        >
+          CONTACT
+        </OptionLink>
+        {currentUser ? (
+          <OptionLink as="div" onClick={() => dispatch(signOutStart())}>
+            SIGN OUT
+          </OptionLink>
+        ) : (
+          <OptionLink
+            active={pathname === "/signin" ? "active" : undefined}
+            to="signin"
+          >
+            SIGN IN
+          </OptionLink>
+        )}
+        <CartIcon />
+      </OptionsContainer>
+      {hidden ? null : <CartDropdown />}
+    </HeaderContainer>
+  );
+}
