b51ee04eaed849bef42fdcb7cad3c94eb505ec5e
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-16 18:34:57 +0100

    Converted App Component to TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, lazy, useState } from "react";
+import { Suspense, useEffect, lazy, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Header from "./components/header/header.component";
@@ -13,16 +13,17 @@ import { fetchCollectionsStart } from "./redux/shop/shop.actions";
 import NotFound from "./components/notfound/notfound.component";
 
 const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
-const SignInAndSignUpPage = lazy(() =>
-  import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
+const SignInAndSignUpPage = lazy(
+  () => import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
 );
 const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"));
 const ContactPage = lazy(() => import("./pages/contact/contactpage.component"));
-const CollectionsOverviewContainer = lazy(() =>
-  import("./components/collections-overview/collections-overview-container")
+const CollectionsOverviewContainer = lazy(
+  () =>
+    import("./components/collections-overview/collections-overview-container")
 );
-const CollectionPageContainer = lazy(() =>
-  import("./components/collection/collection.container")
+const CollectionPageContainer = lazy(
+  () => import("./components/collection/collection.container")
 );
 
 const App = () => {
@@ -41,27 +42,32 @@ const App = () => {
       <div>
         <BrowserRouter>
           <ErrorBoundary>
-            <GlobalStyle />
-            <Header themeToggler={themeToggler} />
-            <Suspense fallback={<Spinner />}>
-              <Routes>
-                <Route exact path="/" element={<HomePage />} />
-                <Route path="/shop">
-                  <Route index element={<CollectionsOverviewContainer />} />
-                  <Route path=":id" element={<CollectionPageContainer />} />
-                </Route>
-                <Route exact path="/checkout" element={<CheckoutPage />} />
-                <Route exact path="/contact" element={<ContactPage />} />
-                <Route
-                  exact
-                  path="/signin"
-                  element={
-                    currentUser ? <Navigate to="/" /> : <SignInAndSignUpPage />
-                  }
-                />
-                <Route path="*" exact={true} element={<NotFound />} />
-              </Routes>
-            </Suspense>
+            <>
+              <GlobalStyle />
+              <Header themeToggler={themeToggler} />
+              <Suspense fallback={<Spinner />}>
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/shop">
+                    <Route index element={<CollectionsOverviewContainer />} />
+                    <Route path=":id" element={<CollectionPageContainer />} />
+                  </Route>
+                  <Route path="/checkout" element={<CheckoutPage />} />
+                  <Route path="/contact" element={<ContactPage />} />
+                  <Route
+                    path="/signin"
+                    element={
+                      currentUser ? (
+                        <Navigate to="/" />
+                      ) : (
+                        <SignInAndSignUpPage />
+                      )
+                    }
+                  />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </Suspense>
+            </>
           </ErrorBoundary>
         </BrowserRouter>
       </div>
