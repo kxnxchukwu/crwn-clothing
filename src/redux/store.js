2cca7d84ad8fe68e6b8ae6b3843f11a9c00b79fb
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-08-16 18:47:25 +0100

    Disabled no-anonymous-default-export eslint rule

--- src/redux/store.js
+++ src/redux/store.js
@@ -20,4 +20,5 @@ sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { store, persistStore };
