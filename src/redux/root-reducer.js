33651ad18c3ed496416a0541cc771f7cb9b19766
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-09-01 15:05:33 +0100

    Updated reducers imports

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,22 +1,24 @@
 import { combineReducers } from "redux";
-import userReducer from "./user/user.reducer";
-import cartReducer from "./cart/cart.reducer";
-import directoryReducer from "./directory/directory.reducer";
+import userReducer from "../features/user-reducer";
+import cartReducer from "../features/cart-slice";
+import directoryReducer from "../features/directory-slice";
+import themeReducer from "../features/theme-slice";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import shopReducer from "./shop/shop.reducer";
+import shopReducer from "../features/shop-slice";
 
 const persistConfig = {
-    key: "root",
-    storage,
-    whitelist: ["cart"]
-}
+  key: "root",
+  storage,
+  whitelist: ["cart", "theme"],
+};
 
 const rootReducer = combineReducers({
-    user: userReducer,
-    cart: cartReducer,
-    directory: directoryReducer,
-    shop: shopReducer
+  user: userReducer,
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
+  theme: themeReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
