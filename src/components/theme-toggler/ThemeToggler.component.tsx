f461bf074110347cbc7f05051be98d4481cd9fdf
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-09-27 22:22:32 +0100

    Hooked ThemeToggler Component to Redux

--- src/components/theme-toggler/ThemeToggler.component.tsx
+++ src/components/theme-toggler/ThemeToggler.component.tsx
@@ -1,21 +1,20 @@
 import { ReactElement } from "react";
 import "./ThemeToggler.styles.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTheme, toggleTheme } from "../../features/theme-slice";
 
-export interface ThemeTogglerProps {
-  themeToggler: () => void;
-}
-
-export default function ThemeToggler({
-  themeToggler,
-}: ThemeTogglerProps): ReactElement {
+export default function ThemeToggler(): ReactElement {
+  const dispatch = useDispatch();
+  const checked = useSelector(selectTheme) === "dark";
   return (
     <>
       {" "}
       <input
-        onClick={() => themeToggler()}
+        onClick={() => dispatch(toggleTheme())}
         type="checkbox"
         className="checkbox"
         id="checkbox"
+        defaultChecked={checked}
       />
       <label htmlFor="checkbox" className="checkbox-label">
         <i className="fas fa-moon"></i>
