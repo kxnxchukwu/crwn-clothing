e21fad83d7869101e64a89d4be2f6b92d76982d9
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-09-27 22:25:04 +0100

    Hooked Cart Icon to RTK

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,13 +1,15 @@
 import { ReactElement } from "react";
 import { ShoppingIcon } from "../shopping-bag/ShoppingBag.component";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleCartHidden } from "../../redux/cart/cart.actions";
-import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+import {
+  toggleCartHidden,
+  selectCartItemsCount,
+} from "../../features/cart-slice";
 import { CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 export default function CartIcon(): ReactElement {
   const dispatch = useDispatch();
-  const itemCount: number = useSelector(selectCartItemsCount);
+  const itemCount = useSelector(selectCartItemsCount);
   const handleClick = () => {
     dispatch(toggleCartHidden());
   };
